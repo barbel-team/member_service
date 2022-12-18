a76c81ea5daaa73af9904f0df5087c18cce77903
bjk1649 <qhwns1649@naver.com>
2022-12-18 19:15:16 +0900

    fix: sql문 수정

--- TeamProject01/sql/create.sql
+++ TeamProject01/sql/create.sql
@@ -47,7 +47,7 @@ INSERT INTO `shoppingmall`.`role` (`name`) VALUES ('ROLE_USER');
 # <!--후기게시판-->
 create table prd_rev (
                          uid INT AUTO_INCREMENT PRIMARY KEY,
-                         fk_member varchar(15) NOT NULL,
+                         fk_member BIGINT NOT NULL,
                          prd_id varchar(30) NOT NULL,
                          b_writer varchar(30) NOT NULL,
                          ment varchar(300) NOT NULL,
@@ -60,21 +60,21 @@ create table prd_rev (
 
 
 # <!--주문관리-->
-create table order (
-                       uid INT AUTO_INCREMENT PRIMARY KEY,
-                       fk_num varchar(15) NOT NULL,
-                       deliv_num varchar(30) NOT NULL,
-                       res_nm varchar(20) NOT NULL,
-                       res_addr01 varchar(30) NOT NULL,
-                       res_addr02 varchar(30) NOT NULL,
-                       res_addr03 varchar(30) NOT NULL,
-                       res_tel varchar(12) NOT NULL,
-                       res_hp varchar(13) NOT NULL,
-                       deliv_req varchar(40) NOT NULL,
-                       ord_date date NOT NULL,
-                       p_time timestamp not null default current_timestamp,
-                       FOREIGN KEY(fk_num) REFERENCES member(uid)
-                           FOREIGN KEY(deliv_num) REFERENCES delivery(uid)
+create table ord (
+    uid INT AUTO_INCREMENT PRIMARY KEY,
+    fk_num BIGINT NOT NULL,
+    deliv_num INT NOT NULL,
+    res_nm varchar(20) NOT NULL,
+    res_addr01 varchar(30) NOT NULL,
+    res_addr02 varchar(30) NOT NULL,
+    res_addr03 varchar(30) NOT NULL,
+    res_tel varchar(12) NOT NULL,
+    res_hp varchar(13) NOT NULL,
+    deliv_req varchar(40) NOT NULL,
+    ord_date date NOT NULL,
+    p_time timestamp not null default current_timestamp,
+    FOREIGN KEY(fk_num) REFERENCES member(uid),
+    FOREIGN KEY(deliv_num) REFERENCES ord_prd(uid)
 );
 
 # <!--주문상품-->
@@ -82,18 +82,15 @@ create table ord_prd (
                          uid INT AUTO_INCREMENT PRIMARY KEY,
                          fk_num varchar(15) NOT NULL,
                          fk_ord_num INT NOT NULL,
-                         fk_adm INT NOT NULL,
-                         FOREIGN KEY(fk_num) REFERENCES member(fk_num)
-                             FOREIGN KEY(fk_ord_num) REFERENCES member(fk_ord_num)
-                             FOREIGN KEY(fk_adm) REFERENCES member(fk_adm)
+                         fk_adm INT NOT NULL
 );
 
 # <!--상품관리-->
 create table product (
                          uid INT AUTO_INCREMENT PRIMARY KEY,
                          fk_ord_num INT NOT NULL,
-                         fk_adm INT NOT NULL,
-                         fk_cate varchar(30) NOT NULL,
+                         fk_adm BIGINT NOT NULL,
+                         fk_cate INT NOT NULL,
                          prd_price varchar(15) NOT NULL,
                          prd_cmp varchar(30) NOT NULL,
                          prd_type varchar(30) NOT NULL,
@@ -102,8 +99,7 @@ create table product (
                          prd_ment varchar(50) NOT NULL,
                          prd_stock INT(15) NOT NULL,
                          prd_sales INT(15) NOT NULL,
-                         FOREIGN KEY(fk_ord_num) REFERENCES ord_prd(fk_ord_num)
-                             FOREIGN KEY(fk_adm) REFERENCES admin(uid)
+                             FOREIGN KEY(fk_adm) REFERENCES role(id),
                              FOREIGN KEY(fk_cate) REFERENCES ctgprd(uid)
 );
 
@@ -122,13 +118,13 @@ create table product_img
 # <!--카테고리별 상품관리-->
 create table ctgprd (
                         uid INT AUTO_INCREMENT PRIMARY KEY,
-                        c_name varchar(30) NOT NULL,
+                        c_name varchar(30) NOT NULL
 );
 
 # <!--문의게시판-->
 create table inquire_board (
                                uid INT AUTO_INCREMENT PRIMARY KEY,
-                               fk_deliv varchar(15) NOT NULL,
+                               fk_deliv INT NOT NULL,
                                b_title varchar(50) NOT NULL,
                                b_pwd varchar(20) NOT NULL,
                                b_writer varchar(15) NOT NULL,
@@ -137,17 +133,17 @@ create table inquire_board (
                                b_cnt INT NOT NULL,
                                b_category varchar(50) NOT NULL,
                                b_lock char(3) NOT NULL,
-                               FOREIGN KEY(fk_deliv) REFERENCES order(uid)
+                               FOREIGN KEY(fk_deliv) REFERENCES ord(uid)
 );
 
 # <!--장바구니-->
 create table cart_prd (
                           uid INT AUTO_INCREMENT PRIMARY KEY,
-                          fk_member INT NOT NULL,
+                          fk_member BIGINT NOT NULL,
                           fk_ord INT NOT NULL,
                           amount INT(20) NOT NULL,
                           cart_chk INT(10) NOT NULL,
                           cart_pay INT(20) NOT NULL,
-                          FOREIGN KEY(fk_member) REFERENCES member(uid)
+                          FOREIGN KEY(fk_member) REFERENCES member(uid),
                               FOREIGN KEY(fk_ord) REFERENCES product(uid)
 );
